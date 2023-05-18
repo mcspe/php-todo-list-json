43b0a624202f09ce11cb8fd136c451ca23bf322b
mcspe <mcspezzaferro@gmail.com>
2023-05-18 14:42:29 +0200

    [add] get json in server and test manipulation

--- php/server.php
+++ php/server.php
@@ -1,3 +1,13 @@
 <?php
-  
+  $toDoString = file_get_contents('toDoList.json');
+  $toDoList = json_decode($toDoString, true);
+
+  $toDoList[] = [
+    'text' => 'fare put content per passarlo al file json',
+    'done' => false
+  ];
+
+  $passString = json_encode($toDoList);
+  file_put_contents('toDoList.json', $passString);
+
 ?>
